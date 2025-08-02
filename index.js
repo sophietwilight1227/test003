21caa18e27b8c29e778c5817abc5370cdd4ee3ed
nanai <hanakoizumian0629@yahoo.co.jp>
2025-08-03 06:20:51 +0900

    test

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
+
 const axios = require('axios');
+const iconv = require('iconv-lite');
 
 app.get('/', async (req, res) => {
   //res.send('Hello World!');
     try {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Content-Type', 'text/plain');
-        const response = await axios.get('https://jbbs.shitaraba.net/bbs/rawmode.cgi/internet/26196/1735542868/');
+        const url = 'https://jbbs.shitaraba.net/bbs/rawmode.cgi/internet/26196/1735542868/'
+        const response = await axios.get(url, {responseType: "arraybuffer",  transformResponse: [ (data) => {
+                                        return iconv.decode(data, 'EUCJP').toString()
+                                    }]})
         res.send(response.data);
     } catch (error) {
         console.error(error);
