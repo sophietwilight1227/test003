bac8557c9adb07f78abe4d59ab33609a6b2e0739
nanai <hanakoizumian0629@yahoo.co.jp>
2025-08-03 05:30:56 +0900

    test

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.get('/', async (req, res) => {
   //res.send('Hello World!');
     try {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Content-Type', 'text/plain');
         const response = await axios.get('https://jbbs.shitaraba.net/bbs/rawmode.cgi/internet/26196/1735542868/');
-        res.text(response.data);
+        res.send(response.data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Something went wrong' });
