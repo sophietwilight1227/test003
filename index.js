12c2041aaace33c5c39cfa89c730ccfaf273c9f9
nanai <hanakoizumian0629@yahoo.co.jp>
2025-08-04 12:47:30 +0900

    test

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 
 const cors = require('cors');
 app.use(cors({
-      origin: 'http://localhost:5173',
       credentials: true,
       optionsSuccessStatus: 200
      }));
